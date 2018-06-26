65220e27bcbd0f36716d27d05f9cb62fc020e548
Ajlal Khan <jalalkhan11223@gmail.com>
2018-06-26 17:51:37 +0500

    user token created successfully

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,253 +2,259 @@
 		CUSTOM FUNCTION WRITE HERE		
 --------------------------------------*/
 "use strict";
-jQuery(document).on('ready', function() {
-	function initGraph(id){
-			setTimeout(function(){
+jQuery(document).on('ready', function () {
+  function initGraph(id) {
+    setTimeout(function () {
 
-			var ctx1 = document.getElementById(id);
-			if(!ctx1) return true
-			var data1 = {
-				labels: ["1", "2", "3", "4",],
-				datasets: [
-				{
-					label: "weekly",
-					backgroundColor: "rgba(242,101,34,1)",
-					borderColor: "rgba(242,101,34,0.8)",
-					pointBorderColor: "rgb(252,176,59)",
-					pointHighlightStroke: "rgba(60,184,120,1)",
-					data: [0, 59, 80, 58, 20, 55, 40]
-				},
-				{
-					label: "weekly",
-					backgroundColor: "rgba(18,62,168,1)",
-					borderColor: "rgba(18,62,168,0.8)",
-					pointBorderColor: "rgb(252,176,59)",
-					pointBackgroundColor: "rgba(221,221,221,1)",
-					data: [28, 48, 40, 19, 86, 27, 90],
-				}
-				
-			]
-			};
-			var areaChart = new Chart(ctx1, {
-				type:"line",
-				data:data1,
-				options: {
-					tooltips: {mode:"label"},
-					elements:{point: {hitRadius:90}},
-					scales: {yAxes: [{stacked: true,gridLines: {color: "#fff",},
-					ticks: {fontFamily: "Poppins", fontColor:"#2f2c2c"}}],
-					xAxes: [{stacked: true,gridLines: {color: "#fff",},
-					ticks: {fontFamily: "Poppins",fontColor:"#2f2c2c"}}]},
-					animation: {duration:	3000},
-					responsive: true,
-					maintainAspectRatio:false,
-					legend: {display: false,},
-					tooltips: {backgroundColor:'rgba(47,44,44,.9)',cornerRadius:0,footerFontFamily:"'Poppins'"}
-				}
-			});
+      var ctx1 = document.getElementById(id);
+      if (!ctx1) return true
+      var data1 = {
+        labels: ["1", "2", "3", "4",],
+        datasets: [
+          {
+            label: "weekly",
+            backgroundColor: "rgba(242,101,34,1)",
+            borderColor: "rgba(242,101,34,0.8)",
+            pointBorderColor: "rgb(252,176,59)",
+            pointHighlightStroke: "rgba(60,184,120,1)",
+            data: [0, 59, 80, 58, 20, 55, 40]
+          },
+          {
+            label: "weekly",
+            backgroundColor: "rgba(18,62,168,1)",
+            borderColor: "rgba(18,62,168,0.8)",
+            pointBorderColor: "rgb(252,176,59)",
+            pointBackgroundColor: "rgba(221,221,221,1)",
+            data: [28, 48, 40, 19, 86, 27, 90],
+          }
 
-			}, 1000)
-		}
+        ]
+      };
+      var areaChart = new Chart(ctx1, {
+        type: "line",
+        data: data1,
+        options: {
+          tooltips: { mode: "label" },
+          elements: { point: { hitRadius: 90 } },
+          scales: {
+            yAxes: [{
+              stacked: true, gridLines: { color: "#fff", },
+              ticks: { fontFamily: "Poppins", fontColor: "#2f2c2c" }
+            }],
+            xAxes: [{
+              stacked: true, gridLines: { color: "#fff", },
+              ticks: { fontFamily: "Poppins", fontColor: "#2f2c2c" }
+            }]
+          },
+          animation: { duration: 3000 },
+          responsive: true,
+          maintainAspectRatio: false,
+          legend: { display: false, },
+          tooltips: { backgroundColor: 'rgba(47,44,44,.9)', cornerRadius: 0, footerFontFamily: "'Poppins'" }
+        }
+      });
+
+    }, 1000)
+  }
 
 	/*--------------------------------------
 			MOBILE MENU						
 	--------------------------------------*/
-	// function collapseMenu(){
-	// 	jQuery('.wp-navigation ul li.menu-item-has-children, .wp-dashboardnav ul li.menu-item-has-children, .wp-navigation ul li.menu-item-has-mega-menu').prepend('<span class="wp-dropdowarrow"><i class="fa fa-angle-down"></i></span>');
-	// 	jQuery('.wp-navigation ul li.menu-item-has-children span, .wp-dashboardnav ul li.menu-item-has-children span, .wp-navigation ul li.menu-item-has-mega-menu span').on('click', function() {
-	// 		jQuery(this).parent('li').toggleClass('wp-open');
-	// 		jQuery(this).next().next().slideToggle(300);
-	// 	});
-	// }
-	// collapseMenu();
+  // function collapseMenu(){
+  // 	jQuery('.wp-navigation ul li.menu-item-has-children, .wp-dashboardnav ul li.menu-item-has-children, .wp-navigation ul li.menu-item-has-mega-menu').prepend('<span class="wp-dropdowarrow"><i class="fa fa-angle-down"></i></span>');
+  // 	jQuery('.wp-navigation ul li.menu-item-has-children span, .wp-dashboardnav ul li.menu-item-has-children span, .wp-navigation ul li.menu-item-has-mega-menu span').on('click', function() {
+  // 		jQuery(this).parent('li').toggleClass('wp-open');
+  // 		jQuery(this).next().next().slideToggle(300);
+  // 	});
+  // }
+  // collapseMenu();
 	/*--------------------------------------
 			MOBILE MENU						
 	--------------------------------------*/
-	function collapseMenu(){
-		jQuery('.wp-navigation ul li.menu-item-has-children, .wp-dashboardnav ul li.menu-item-has-children').prepend('<span class="wp-dropdowarrow"><i class="fa fa-angle-down"></i></span>');
-		jQuery('.wp-navigation ul li.menu-item-has-children span, .wp-dashboardnav ul li.menu-item-has-children a').on('click', function() {
-			jQuery(this).parent('li').toggleClass('wp-open');
-			jQuery(this).next().slideToggle(300);
-		});
-	}
-	collapseMenu();
+  function collapseMenu() {
+    jQuery('.wp-navigation ul li.menu-item-has-children, .wp-dashboardnav ul li.menu-item-has-children').prepend('<span class="wp-dropdowarrow"><i class="fa fa-angle-down"></i></span>');
+    jQuery('.wp-navigation ul li.menu-item-has-children span, .wp-dashboardnav ul li.menu-item-has-children a').on('click', function () {
+      jQuery(this).parent('li').toggleClass('wp-open');
+      jQuery(this).next().slideToggle(300);
+    });
+  }
+  collapseMenu();
 	/*--------------------------------------
 			MEGA MENU						
 	--------------------------------------*/
-	jQuery(function ($) {
-		function hoverIn() {
-			var a = jQuery(this);
-			var nav = a.closest('.wp-navigation');
-			var mega = a.find('.mega-menu');
-			var offset = rightSide(nav) - leftSide(a);
-			mega.width(Math.min(rightSide(nav), columns(mega)*230));
-			mega.css('left', Math.min(0, offset - mega.width()));
-		}
-		function hoverOut() {}
-		function columns(mega) {
-			var columns = 0;
-			mega.children('.mega-menu-row').each(function () {
-				columns = Math.max(columns, jQuery(this).children('.mega-menu-col').length);
-			});
-			return columns;
-		}
-		function leftSide(elem) {
-			return elem.offset().left;
-		}
-		function rightSide(elem) {
-			return elem.offset().left + elem.width();
-		}
-		jQuery('.menu-item-has-mega-menu').hover(hoverIn, hoverOut);
-	});
+  jQuery(function ($) {
+    function hoverIn() {
+      var a = jQuery(this);
+      var nav = a.closest('.wp-navigation');
+      var mega = a.find('.mega-menu');
+      var offset = rightSide(nav) - leftSide(a);
+      mega.width(Math.min(rightSide(nav), columns(mega) * 230));
+      mega.css('left', Math.min(0, offset - mega.width()));
+    }
+    function hoverOut() { }
+    function columns(mega) {
+      var columns = 0;
+      mega.children('.mega-menu-row').each(function () {
+        columns = Math.max(columns, jQuery(this).children('.mega-menu-col').length);
+      });
+      return columns;
+    }
+    function leftSide(elem) {
+      return elem.offset().left;
+    }
+    function rightSide(elem) {
+      return elem.offset().left + elem.width();
+    }
+    jQuery('.menu-item-has-mega-menu').hover(hoverIn, hoverOut);
+  });
 	/*--------------------------------------
 			DASHBOARD MENU					
 	--------------------------------------*/
-	if(jQuery('#wp-closenav').length > 0){
-		jQuery("#wp-closenav").on('click', function(event) {
-			event.preventDefault();
-			jQuery('#wp-wrapper').toggleClass('wp-openmenu');
-			jQuery('body').toggleClass('wp-noscroll');
-			jQuery('.wp-sidebarwrapper ul.sub-menu').hide();
-		});
-	}
+  if (jQuery('#wp-closenav').length > 0) {
+    jQuery("#wp-closenav").on('click', function (event) {
+      event.preventDefault();
+      jQuery('#wp-wrapper').toggleClass('wp-openmenu');
+      jQuery('body').toggleClass('wp-noscroll');
+      jQuery('.wp-sidebarwrapper ul.sub-menu').hide();
+    });
+  }
 	/* -------------------------------------
 			COUNTER
 	-------------------------------------- */
-	var _wp_counters = jQuery('#wp-statisticscounters');
-	_wp_counters.appear(function () {
-		var _wp_counter = jQuery('.wp-countercontent h3 span');
-		_wp_counter.countTo();
-	});
-	
+  var _wp_counters = jQuery('#wp-statisticscounters');
+  _wp_counters.appear(function () {
+    var _wp_counter = jQuery('.wp-countercontent h3 span');
+    _wp_counter.countTo();
+  });
+
 	/*--------------------------------------
 			THEME VERTICAL SCROLLBAR		
 	--------------------------------------*/
-	if(jQuery('.wp-verticalscrollbar').length > 0){
-		var _tg_verticalscrollbar = jQuery('.wp-verticalscrollbar');
-		_tg_verticalscrollbar.mCustomScrollbar({
-			axis:"y",
-		});
-	}
-	if(jQuery('.wp-horizontalthemescrollbar').length > 0){
-		var _tg_horizontalthemescrollbar = jQuery('.wp-horizontalthemescrollbar');
-		_tg_horizontalthemescrollbar.mCustomScrollbar({
-			axis:"x",
-			advanced:{autoExpandHorizontalScroll:true},
-		});
-	}
+  if (jQuery('.wp-verticalscrollbar').length > 0) {
+    var _tg_verticalscrollbar = jQuery('.wp-verticalscrollbar');
+    _tg_verticalscrollbar.mCustomScrollbar({
+      axis: "y",
+    });
+  }
+  if (jQuery('.wp-horizontalthemescrollbar').length > 0) {
+    var _tg_horizontalthemescrollbar = jQuery('.wp-horizontalthemescrollbar');
+    _tg_horizontalthemescrollbar.mCustomScrollbar({
+      axis: "x",
+      advanced: { autoExpandHorizontalScroll: true },
+    });
+  }
 	/*--------------------------------------
 			THEME VERTICAL SCROLLBAR		
 	--------------------------------------*/
-	// function init_chart(){
-		// var ctx1 = document.getElementById("high-canvas");
-		// var data1 = {
-		// 	labels: ["1", "2", "3", "4",],
-		// 	datasets: [
-		// 		{
-		// 			label: "weekly",
-		// 			backgroundColor: "rgba(242,101,34,1)",
-		// 			borderColor: "rgba(242,101,34,0.8)",
-		// 			pointBorderColor: "rgb(252,176,59)",
-		// 			pointHighlightStroke: "rgba(60,184,120,1)",
-		// 			data: [0, 59, 80, 58, 20, 55, 40]
-		// 		},
-		// 		{
-		// 			label: "weekly",
-		// 			backgroundColor: "rgba(18,62,168,1)",
-		// 			borderColor: "rgba(18,62,168,0.8)",
-		// 			pointBorderColor: "rgb(252,176,59)",
-		// 			pointBackgroundColor: "rgba(221,221,221,1)",
-		// 			data: [28, 48, 40, 19, 86, 27, 90],
-		// 		}
-				
-		// 	]
-		// };
+  // function init_chart(){
+  // var ctx1 = document.getElementById("high-canvas");
+  // var data1 = {
+  // 	labels: ["1", "2", "3", "4",],
+  // 	datasets: [
+  // 		{
+  // 			label: "weekly",
+  // 			backgroundColor: "rgba(242,101,34,1)",
+  // 			borderColor: "rgba(242,101,34,0.8)",
+  // 			pointBorderColor: "rgb(252,176,59)",
+  // 			pointHighlightStroke: "rgba(60,184,120,1)",
+  // 			data: [0, 59, 80, 58, 20, 55, 40]
+  // 		},
+  // 		{
+  // 			label: "weekly",
+  // 			backgroundColor: "rgba(18,62,168,1)",
+  // 			borderColor: "rgba(18,62,168,0.8)",
+  // 			pointBorderColor: "rgb(252,176,59)",
+  // 			pointBackgroundColor: "rgba(221,221,221,1)",
+  // 			data: [28, 48, 40, 19, 86, 27, 90],
+  // 		}
 
-// setTimeout(function(){
-	initGraph('high-canvas')
-// }, 3000)
-		// var areaChart = new Chart(ctx1, {
-		// 	type:"line",
-		// 	data:data1,
-		// 	options: {
-		// 		tooltips: {mode:"label"},
-		// 		elements:{point: {hitRadius:90}},
-		// 		scales: {yAxes: [{stacked: true,gridLines: {color: "#fff",},
-		// 		ticks: {fontFamily: "Poppins", fontColor:"#2f2c2c"}}],
-		// 		xAxes: [{stacked: true,gridLines: {color: "#fff",},
-		// 		ticks: {fontFamily: "Poppins",fontColor:"#2f2c2c"}}]},
-		// 		animation: {duration:	3000},
-		// 		responsive: true,
-		// 		maintainAspectRatio:false,
-		// 		legend: {display: false,},
-		// 		tooltips: {backgroundColor:'rgba(47,44,44,.9)',cornerRadius:0,footerFontFamily:"'Poppins'"}
-		// 	}
-		// });
-	// }
-	// 
-	// init_chart();
-		
-		/*--------------------------------------
-				DATA TABLE		
-		--------------------------------------*/
-		if(jQuery('table.display').length > 0){
-			var _display = jQuery('table.display').DataTable({
-				 scrollY: 400,
-			});
-		}
-		/*--------------------------------------
-				TINYMCE WYSIWYG EDITOR			
-		--------------------------------------*/
-		if(jQuery('#wp-tinymceeditor').length > 0){
-			tinymce.init({
-				selector: 'textarea#wp-tinymceeditor',
-				height: 250,
-				theme: 'modern',
-				plugins: [ 'advlist autolink lists link image charmap print preview hr anchor pagebreak', 'searchreplace wordcount visualblocks visualchars code fullscreen', 'insertdatetime media nonbreaking save table contextmenu directionality', 'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'],
-				toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-				toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
-				image_advtab: true,
-			});
-		}
+  // 	]
+  // };
 
-		
+  // setTimeout(function(){
+  initGraph('high-canvas')
+  // }, 3000)
+  // var areaChart = new Chart(ctx1, {
+  // 	type:"line",
+  // 	data:data1,
+  // 	options: {
+  // 		tooltips: {mode:"label"},
+  // 		elements:{point: {hitRadius:90}},
+  // 		scales: {yAxes: [{stacked: true,gridLines: {color: "#fff",},
+  // 		ticks: {fontFamily: "Poppins", fontColor:"#2f2c2c"}}],
+  // 		xAxes: [{stacked: true,gridLines: {color: "#fff",},
+  // 		ticks: {fontFamily: "Poppins",fontColor:"#2f2c2c"}}]},
+  // 		animation: {duration:	3000},
+  // 		responsive: true,
+  // 		maintainAspectRatio:false,
+  // 		legend: {display: false,},
+  // 		tooltips: {backgroundColor:'rgba(47,44,44,.9)',cornerRadius:0,footerFontFamily:"'Poppins'"}
+  // 	}
+  // });
+  // }
+  // 
+  // init_chart();
+
+  /*--------------------------------------
+      DATA TABLE		
+  --------------------------------------*/
+  if (jQuery('table.display').length > 0) {
+    var _display = jQuery('table.display').DataTable({
+      scrollY: 400,
+    });
+  }
+  /*--------------------------------------
+      TINYMCE WYSIWYG EDITOR			
+  --------------------------------------*/
+  if (jQuery('#wp-tinymceeditor').length > 0) {
+    tinymce.init({
+      selector: 'textarea#wp-tinymceeditor',
+      height: 250,
+      theme: 'modern',
+      plugins: ['advlist autolink lists link image charmap print preview hr anchor pagebreak', 'searchreplace wordcount visualblocks visualchars code fullscreen', 'insertdatetime media nonbreaking save table contextmenu directionality', 'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'],
+      toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+      toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
+      image_advtab: true,
+    });
+  }
+
+
 	/*--------------------------------------
 			Google Map
 	--------------------------------------*/
-	if(jQuery('#wp-locationmap').length > 0){
-		var _wp_locationmap = jQuery('#wp-locationmap');
-		_wp_locationmap.gmap3({
-			marker: {
-				address: '1600 Elizabeth St, Melbourne, Victoria, Australia',
-				options: {
-					title: 'Robert Frost Elementary School'
-				}
-			},
-			map: {
-				options: {
-					zoom: 16,
-					scrollwheel: false,
-					disableDoubleClickZoom: true,
-				}
-			}
-		});
-	}
+  if (jQuery('#wp-locationmap').length > 0) {
+    var _wp_locationmap = jQuery('#wp-locationmap');
+    _wp_locationmap.gmap3({
+      marker: {
+        address: '1600 Elizabeth St, Melbourne, Victoria, Australia',
+        options: {
+          title: 'Robert Frost Elementary School'
+        }
+      },
+      map: {
+        options: {
+          zoom: 16,
+          scrollwheel: false,
+          disableDoubleClickZoom: true,
+        }
+      }
+    });
+  }
 	/*--------------------------------------
 			PRETTY PHOTO GALLERY			
 	--------------------------------------*/
-	if(jQuery('#wp-prettyphoto').length > 0){
-		jQuery("a[data-rel]").each(function () {
-			jQuery(this).attr("rel", jQuery(this).data("rel"));
-		});
-		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
-			animation_speed: 'normal',
-			theme: 'dark_square',
-			slideshow: 3000,
-			autoplay_slideshow: false,
-			social_tools: false
-		});
-	}
+  if (jQuery('#wp-prettyphoto').length > 0) {
+    jQuery("a[data-rel]").each(function () {
+      jQuery(this).attr("rel", jQuery(this).data("rel"));
+    });
+    jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
+      animation_speed: 'normal',
+      theme: 'dark_square',
+      slideshow: 3000,
+      autoplay_slideshow: false,
+      social_tools: false
+    });
+  }
 });
 
 
@@ -271,7 +277,7 @@ function userLogin() {
           } else if (res.success.role === 'admin') {
             window.location.href = '/admin/dashboard';
           }
-          else{
+          else {
             window.location.href = '/users/dashboard';
           }
         }
@@ -354,16 +360,14 @@ function addUser() {
 // FORGOT PASSWORD FUNCTION
 
 function forgotpassword() {
-  document.getElementById('btnSubmit').disabled = true;
   var email = document.getElementById('email').value;
-  if (email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
     $.ajax({
       type: "POST",
       url: "/forgotpassword/forgetPassword",
       data: { email: email },
       success: function (res) {
         if (res.error) {
-          document.getElementById('btnSubmit').disabled = false;
           var error = document.getElementById('error');
           error.style.color = 'red'
           error.innerHTML = 'User with this email doesn\'t exists';
@@ -375,7 +379,6 @@ function forgotpassword() {
       }
     });
   } else {
-    document.getElementById('btnSubmit').disabled = false;
     var error = document.getElementById('error');
     error.style.color = 'red'
     error.innerHTML = 'Please enter an email address.'
