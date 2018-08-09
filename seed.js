0bd78d15eb64374fae93cf8b74ac9ab3d4c1adca
Ajlal Khan <jalalkhan11223@gmail.com>
2018-08-09 15:38:08 +0500

    new bd url added

--- seed.js
+++ seed.js
@@ -2,8 +2,8 @@ var User = require('./models/user')
 var mongoose = require("mongoose");
 
 //mongo connection
-// mongoose.connect('mongodb://maxifjaved:maxifjaved@127.0.0.1:27017/carsOnline?authSource=admin')
-mongoose.connect('mongodb://127.0.0.1:27017/carsOnline')
+mongoose.connect('mongodb://maxifjaved:maxifjaved@127.0.0.1:27017/carsOnline?authSource=admin')
+// mongoose.connect('mongodb://127.0.0.1:27017/carsOnline')
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
